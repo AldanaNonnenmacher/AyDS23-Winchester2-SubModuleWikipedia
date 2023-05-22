623f9adc5192d5160195cf6ebd48ab725ece786a
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2023-05-22 11:06:47 -0300

    Create WikipediaInjector.kt

---
+++ src/main/java/ayds/winchester2/wikipediaexternal/injector/WikipediaInjector.kt
@@ -0,0 +1,31 @@
+package ayds.winchester2.wikipediaexternal.injector
+
+
+import ayds.winchester2.wikipediaexternal.data.wikipedia.WikipediaTrackService
+import ayds.winchester2.wikipediaexternal.data.wikipedia.tracks.JsonToInfoResolver
+import ayds.winchester2.wikipediaexternal.data.wikipedia.tracks.WikipediaToInfoResolver
+import ayds.winchester2.wikipediaexternal.data.wikipedia.tracks.WikipediaTrackAPI
+import ayds.winchester2.wikipediaexternal.data.wikipedia.tracks.WikipediaTrackServiceImpl
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val WIKIPEDIA_URL = "https://en.wikipedia.org/w/"
+
+object WikipediaInjector {
+    private val wikipediaAPIRetrofit = getRetrofit()
+    private val wikipediaTrackAPI = getWikipediaAPI(wikipediaAPIRetrofit)
+    private val wikipediaToInfoResolver: WikipediaToInfoResolver = JsonToInfoResolver()
+
+    val wikipediaTrackService: WikipediaTrackService =
+        WikipediaTrackServiceImpl(
+            wikipediaTrackAPI,
+            wikipediaToInfoResolver
+        )
+
+    private fun getRetrofit() = Retrofit.Builder()
+        .baseUrl(WIKIPEDIA_URL)
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()
+
+    private fun getWikipediaAPI(retrofit: Retrofit) = retrofit.create(WikipediaTrackAPI::class.java)
+}
